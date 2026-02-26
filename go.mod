3a1286e08266e4f9d8e26efff83dfa25f6a07e29
Akira Masuda <masuda.akira1014@gmail.com>
2026-02-26 10:06:34 +0900

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/go-zen-chu/golang-template
+
+go 1.25
+
+require github.com/go-zen-chu/go-build-tools v0.0.0-20240929035559-91e20c6456d7
---
+++ magefile.go
@@ -0,0 +1,59 @@
+//go:build mage
+// +build mage
+
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+
+	gbt "github.com/go-zen-chu/go-build-tools"
+)
+
+const imageRegistry = "amasuda"
+const repository = "golang-template"
+const dockerFileLocation = "."
+
+func init() {
+	// by default, magefile does not output stderr
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+}
+
+/*=======================
+setup
+=======================*/
+
+// InstallDevTools installs required development tools for this project
+func InstallDevTools() error {
+	outMsg, errMsg, err := gbt.RunLongRunningCmdWithLog("go install go.uber.org/mock/mockgen@latest")
+	if err != nil {
+		return fmt.Errorf("installing mockgen: %w\nstdout: %s\nstderr: %s\n", err, outMsg, errMsg)
+	}
+	outMsg, errMsg, err = gbt.RunLongRunningCmdWithLog("brew install golangci-lint")
+	if err != nil {
+		return fmt.Errorf("installing mockgen: %w\nstdout: %s\nstderr: %s\n", err, outMsg, errMsg)
+	}
+	return nil
+}
+
+/*=======================
+workflow
+=======================*/
+
+func DockerLogin() error {
+	return gbt.DockerLogin()
+}
+
+func DockerBuildLatest() error {
+	return gbt.DockerBuildLatest(imageRegistry, repository, dockerFileLocation)
+}
+
+func DockerPublishLatest() error {
+	return gbt.DockerPublishLatest(imageRegistry, repository)
+}
+
+func DockerBuildPublishWithGenTag() error {
+	return gbt.DockerBuildPublishGeneratedImageTag(imageRegistry, repository, dockerFileLocation)
+}
